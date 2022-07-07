6745b52f291735e9671a025a3b5e97648fac73cb
Michał Wójcik <m.mwojcik@outlook.com>
2022-07-07 18:40:39 +0200

    finish requirements with table and fetching data using client service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { ExchangeRatesClientService } from '../services/exchange-rates-client.service';
+import { catchError, map, tap } from 'rxjs/operators';
+import { ExchangeResultsItemDto } from '../models/exchange-results.dto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'exchange-rates-viewer';
+export class AppComponent implements OnInit {
+  exchangeRateValues$?: Observable<ExchangeResultsItemDto[]>;
+  loading = false;
+  searchDate?: Date;
+  errorMessage?: string;
+
+  minDate = new Date(new Date().setDate(-7));
+  maxDate = new Date();
+
+  constructor(private readonly _clientService: ExchangeRatesClientService) {
+  }
+
+  ngOnInit(): void {
+    this._fetchResults()
+  }
+
+  onClearClicked(): void {
+    this.searchDate = undefined;
+    this._fetchResults();
+  }
+
+  onDateChanged(searchDate?: Date): void {
+    this._fetchResults(searchDate);
+  }
+
+  private _fetchResults(searchDate?: Date): void {
+    this.loading = true;
+    this.errorMessage = '';
+    const isValidDate = searchDate && !isNaN(searchDate.getTime());
+
+    if (searchDate && !isValidDate) {
+      this.errorMessage = 'Invalid date';
+      this.exchangeRateValues$ = of([]);
+      return;
+    }
+
+    const request = !searchDate ? this._clientService.getLatestRates$() :
+      this._clientService.getExchangeRatesByDate$(new Date(searchDate))
+
+    this.exchangeRateValues$ = request.pipe(
+      map(result => {
+        this.loading = false;
+        return result.length == 1 ? result[0].rates : [];
+      }),
+      tap(result => {
+        this.errorMessage = result.length === 0 ? 'No results found' : undefined;
+      }),
+      catchError(err => {
+        this.loading = false;
+        this.errorMessage = `Backend error: ${err.statusText}`;
+        return throwError(err);
+      })
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { ThemeSelectorComponent } from './components/theme-selector/theme-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeSelectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    ButtonModule,
+    HttpClientModule,
+    InputTextModule,
+    CalendarModule,
+    FormsModule,
+    SelectButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
